1c963911f1919ed0ac1d938fe7216740d9e35c1e
BMAgeas <Ben.Mullan@Ageas.Co.UK>
2023-11-02 09:30:36 +0000

    StormHub New Images

--- src/components/StormAdviceItem/StormAdviceItem.jsx
+++ src/components/StormAdviceItem/StormAdviceItem.jsx
@@ -10,7 +10,7 @@ const StormAdviceItem = ({ Text  }) => {
 	return (
 		<li className={styles.StormAdviceItem}>
 			<figure>
-				<Image src="/StormCar.jfif" alt="Advice!" width={0} height={0} sizes="100vw" style={{ width : "100%", height : "auto" }} className={styles.StormAdviceImage} />
+				<Image src={`/StormImages/Storm%20(${Math.floor(Math.random() * 10).toString()}).jpg`} alt="Advice!" width={0} height={0} sizes="100vw" style={{ width : "100%", height : "auto" }} className={styles.StormAdviceImage} />
 				<figcaption>
 					<h6 className={styles.AdviceItemHeading}><strong>{Text.split(" - ")[0]}</strong></h6>
 					{Text.split(" - ")[1]}
