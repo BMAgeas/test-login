489be34077ecfc1de1ca7163f2b211ff351e4307
BMAgeas <Ben.Mullan@Ageas.Co.UK>
2023-11-09 12:00:32 +0000

    Removed BOOTSTRAP

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import Head   from "next/head";
 import Script from "next/script";
 
-import "bootstrap/dist/css/bootstrap.css";
 import "./globals.css";
 import "./globals.scss";
 
@@ -10,7 +9,6 @@ export default function RootLayout({ children }) {
 		<html lang="en">
 			<Head>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				{/* <Script src="/bootstrap.bundle.min.js" onLoad={() => { console.log("Next loaded BSJS"); }} /> */}
 			</Head>
 			<body>{children}</body>
 		</html>
