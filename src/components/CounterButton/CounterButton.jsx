aa0dba101b698276d8c2280e4b0a94dcb5ca3a63
BMAgeas <Ben.Mullan@Ageas.Co.UK>
2023-11-09 09:10:10 +0000

    Implemented CounterButton with useState()

--- src/components/CounterButton/CounterButton.jsx
+++ src/components/CounterButton/CounterButton.jsx
@@ -1,8 +1,21 @@
-import styles from "./CounterButton.module.scss";
+import {useState}	from "react";
+import styles		from "./CounterButton.module.scss";
 
 const CounterButton = () => {
+
+	/*
+		clickCount		→ The current state
+		setClickCount	→ A function for updating the state
+		0				→ The initial value
+	*/
+	const [clickCount, setClickCount] = useState(0);
+
+	function UpdateClickCount() {
+		setClickCount(prev => prev + 1);
+	}
+
     return (
-        <button className={`${styles.CounterButton} btn btn-primary clicky-button my-2 px-4`}>Clicked * time</button>
+        <button onClick={UpdateClickCount} className={`${styles.CounterButton} btn btn-primary clicky-button my-2 px-4`}>Clicked {clickCount.toString()} time{(clickCount > 1) && "s"}</button>
     );
 };
 
